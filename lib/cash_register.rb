af69b5eb93499623e8f7a825a28f39d377d36787
Roberto Medina <robby.medina562@gmail.com>
2019-09-17 16:57:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,17 +10,7 @@ class CashRegister
   def add_item(item, price, quantity = 0)
     @price = (price * quantity)
     @total += @price
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
-    end
-  end
-
+    quantity.times 
   def apply_discount
     if @discount > 0
       @to_take_off = (price * discount)/100
